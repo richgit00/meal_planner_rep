ec78f95c2615d7dd844b0c61e18b4f98f89e8156
richgit00 <edwards_ra@yahoo.co.uk>
2025-07-28 11:27:59 +0000

    Your commit message

--- server/index.ts
+++ server/index.ts
@@ -82,10 +82,11 @@ app.use((req, res, next) => {
   // Other ports are firewalled. Default to 5000 if not specified.
   // this serves both the API and the client.
   // It is the only port that is not firewalled.
-  const port = parseInt(process.env.PORT!, 10); // PORT is required by Render
+  const port = parseInt(process.env.PORT || '5000', 10);
   const server = app.listen(port, '0.0.0.0', () => {
-    log(`✅ Server running on port ${port} (Render-compliant)`);
+    log(`serving on port ${port} (accessible on 0.0.0.0)`);
   });
+
 
 
   // importantly only setup vite in development and after
