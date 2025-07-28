f5f35098d2a014ccac0e86c9c18d10506aceb4cf
richgit00 <edwards_ra@yahoo.co.uk>
2025-07-28 11:06:52 +0000

    Your commit message

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,4 @@
-// this is new
 import { Router as WouterRouter, Switch, Route } from "wouter";
-//import { Switch, Route, Redirect } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
@@ -13,25 +11,11 @@ import Pantry from "@/pages/pantry";
 import Admin from "@/pages/admin";
 import NotFound from "@/pages/not-found";
 
-function Router() {
-  return (
-    <Switch>
-      <Route path="/" component={Home} exact />
-      <Route path="/meal-plan" component={MealPlan} />
-      <Route path="/shopping-list" component={ShoppingList} />
-      <Route path="/pantry" component={Pantry} />
-      <Route path="/admin" component={Admin} />
-      <Route component={NotFound} />
-    </Switch>
-  );
-}
-
-// this is new
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <TooltipProvider>
-        <WouterRouter base="/">
+        <WouterRouter>
           <div className="min-h-screen bg-slate-50">
             <Header />
             <main>
@@ -51,25 +35,5 @@ function App() {
     </QueryClientProvider>
   );
 }
-
-
-
-
-
-/*function App() {
-  return (
-    <QueryClientProvider client={queryClient}>
-      <TooltipProvider>
-        <div className="min-h-screen bg-slate-50">
-          <Header />
-          <main>
-            <Router />
-          </main>
-        </div>
-        <Toaster />
-      </TooltipProvider>
-    </QueryClientProvider>
-  );
-}*/
 
 export default App;
